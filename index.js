f56ad1b8cde7b8840a98a0e5b44958407a6b86ce
stkirk <shaanekirk@gmail.com>
2021-05-12 17:56:52 -0400

    completed task 3, write a class that takes a single arguement

--- index.js
+++ index.js
@@ -107,7 +107,16 @@ class Car {
           + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
           + {name} and {location} of course come from the instance's own properties.
   */
-class Lambdasian {}
+class Lambdasian {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, and I am from ${this.location}.`;
+  }
+}
 
 /*
     TASK 4
